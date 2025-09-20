4485bad566088ff456c3f5ffb515a13585ab6b59
AbdullahRana11 <mabdullahrana12@gmail.com>
2025-09-20 11:12:20 +0500

    Dynamic String Reversal with GIT workflow

--- string_reverse.cpp
+++ string_reverse.cpp
@@ -10,13 +10,19 @@ int main() {
     int size= (*(str_reverse)).length();
 
     for(int i=0; i<size/2;i++){
-        char temp= (*str_reverse)[i];
-        (*str_reverse)[i] = (*str_reverse)[size - i - 1];
+
+        // store current character in a temporary variable
+        char temp= (*str_reverse)[i];  
+
+        // replace character at current index with its opposite from the end
+        (*str_reverse)[i] = (*str_reverse)[size - i - 1]; 
+
+        // put the saved character into the opposite position
         (*str_reverse)[size - i - 1] = temp;
     }
 
     cout<<"The Reversed String is:      '"<<*str_reverse<<"'"<<endl;
 
-    delete str_reverse;
+    delete str_reverse;  //free dynamically allocated memory
     return 0;
 }
