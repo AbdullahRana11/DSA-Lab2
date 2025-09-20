44df7ba795421b54dcb37b133efe550dd9c577cf
AbdullahRana11 <mabdullahrana12@gmail.com>
2025-09-20 11:10:45 +0500

    Analyze Pointer Program with GIT workflow

--- analyze_pointer.cpp
+++ analyze_pointer.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 void analyze_pointer(int *ptr){
-	cout << "Memory Address: " << ptr << endl;
-	cout << "Value at Address: " << *ptr << endl;
+	cout << "Memory Address: " << ptr << endl;           //prints address
+	cout << "Value at Address: " << *ptr << endl;       //prints value
 }
 
 int main() {
 	int iValue = 56;
-	analyze_pointer(&iValue);
-	int *new_value = new int;
+	analyze_pointer(&iValue);    // Pass the address of a stack variable to the function
+
+	int *new_value = new int;    // Dynamically allocate memory for an integer
 	*new_value = 78;
 	analyze_pointer(new_value);
+
+	delete new_value;           // free dynamically allocated memory
 	return 0;
 }
